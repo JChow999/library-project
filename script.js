8a8a957def44a6f8d10a70eece07beb9ba74b06d
Jason <123558105+JChow999@users.noreply.github.com>
2023-11-21 13:50:03 -0500

    Add js logic to allow user to add and remove books properly

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const libraryDisp = document.querySelector(".libraryDisp")
+const libraryDisp = document.querySelector(".library-display")
 
 const newBookBtn = document.querySelector("#new-book-btn");
 const bookFormModal = document.querySelector("#book-form-modal");
@@ -13,7 +13,7 @@ const bookSubmit = document.querySelector("#submit-book")
 
 const myLibrary = [];
 
-
+// Book Constructor
 function Book(title, author, pages, read_status) {
     this.title = title;
     this.author = author;
@@ -21,14 +21,13 @@ function Book(title, author, pages, read_status) {
     this.read_status = read_status;
 }
 
-
-
+// Event listeners
 newBookBtn.addEventListener('click', () => {
     bookFormModal.showModal();
 })
 
 document.addEventListener('click', function(e) {
-    console.log(e.target)
+    // console.log(e.target)
     if (e.target === bookFormModal || e.target.classList.contains("close-form")) {
         bookFormModal.close();
     }
@@ -55,34 +54,66 @@ function checkBookValidity() {
     }
 }
 
-function showBookOnSite() {
-    console.log();
-    let bookDisp = document.createElement(div);
-    let bookDispTitle = document.createElement(h2);
-
-    bookDisp.className = "book";
-}
-
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
     bookTitle.value = "";
     bookAuthor.value = "";
     bookPages.value = "";
     bookRead.value = "";
+    
+    updateBookDisplay();
     console.log(myLibrary);
 }
 
-function updateBooks() {
-
+function updateBookDisplay() {
+    libraryDisp.innerHTML = "";
+    // console.log(bookInfo);
+    for (let n = 0; n < myLibrary.length; n++) {
+        let bookInfo = myLibrary[n];
+        
+        const newBookDisp = document.createElement('div');
+        const newBookTitle = document.createElement("h3");
+        const newBookPara = document.createElement("p");
+        const newBookLabel = document.createElement("label");
+        const newBookCheck = document.createElement("input");
+        const newBookRemoveBtn = document.createElement("input");
+        
+        newBookDisp.setAttribute("class", "book");
+        newBookDisp.setAttribute("data-index", myLibrary.indexOf(bookInfo));
+        newBookTitle.textContent = `${bookInfo.title} by ${bookInfo.author}`;
+        newBookPara.textContent = `Pages: ${bookInfo.pages}`;
+        newBookLabel.textContent = "Book Read";
+        
+        newBookCheck.type = "checkbox";
+        newBookCheck.checked = bookInfo.read_status;   
+        newBookRemoveBtn.value = "Delete Book";
+        
+        newBookCheck.addEventListener("click", () => {
+            console.log(newBookCheck.checked);
+            myLibrary[newBookDisp.getAttribute("data-index")].read_status = newBookCheck.checked;
+        })
+        
+        
+        newBookRemoveBtn.addEventListener('click', () => {
+            myLibrary.splice(newBookDisp.getAttribute("data-index"), 1);
+            libraryDisp.removeChild(newBookDisp);
+            console.log(myLibrary);
+            updateBookDisplay();
+        })
+        
+        newBookDisp.appendChild(newBookTitle);
+        newBookDisp.appendChild(newBookPara);
+        newBookDisp.appendChild(newBookLabel);
+        newBookDisp.appendChild(newBookCheck);
+        newBookDisp.appendChild(newBookRemoveBtn);
+        
+        libraryDisp.appendChild(newBookDisp);
+    }
 }
 
-function removeBook() {
-
-}
-
-function changeReadStatus() {
-
-}
-
-let hobbit = new Book("Hobbit", "Tolken", 200, true)
+let hobbit = new Book("Hobbit", "J.R.R Tolken", 200, true)
+let harryPot = new Book("Harry Potter", "J.K. Rowling", 463, false);
+let levWakes = new Book("Leviathan Wakes", "James S. A. Corey", 577, true);
 addBookToLibrary(hobbit)
+addBookToLibrary(harryPot)
+addBookToLibrary(levWakes)
